f3f82349cdc7301cc486f3da7b408e1c95ba96a0
Kiruthika M <kiruthikam.22eie@kongu.edu>
2025-04-30 22:00:49 +0530

    Time: 0 ms (100%), Space: 40.3 MB (65.38%) - LeetHub

---
+++ 0292-nim-game/0292-nim-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canWinNim(int n) {
+        int m=n%4;
+        if(m==0){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
