ee7015ff8a30bf49f8962c118f0feef849c7a587
Kiruthika M <kiruthikam.22eie@kongu.edu>
2025-05-04 22:31:22 +0530

    Time: 0 ms (100%), Space: 41.1 MB (96.65%) - LeetHub

--- 0909-stone-game/0909-stone-game.java
+++ 0909-stone-game/0909-stone-game.java
@@ -1,11 +1,13 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        for(int i=0;i < piles.length;i++){
+        /*for(int i=0;i < piles.length;i++){
            if( piles[i]%2==0){
             return true;
            }
            
         }
         return false;
+        */
+        return true;
     }
 }
