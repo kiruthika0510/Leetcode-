16c9b8b96d1fc1d2cf6fa11fefb732d2d00cf3f9
Kiruthika M <kiruthikam.22eie@kongu.edu>
2025-05-07 17:13:46 +0530

    Time: 2 ms (71.18%), Space: 45.4 MB (47.06%) - LeetHub

---
+++ 0978-valid-mountain-array/0978-valid-mountain-array.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean validMountainArray(int[] a) {
+        int n=a.length;
+        int i=0;
+        if(n<3){
+            return false;
+        }
+        while(i+1<n && a[i]<a[i+1]){
+        i++;
+        }
+        if(i==0 || i == n-1){
+            return false;
+        }
+        while(i+1<n && a[i]>a[i+1]){
+            i++;
+        }
+       return i==n-1 ;
+    }
+}
